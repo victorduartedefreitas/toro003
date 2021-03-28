24b71ecf78dbbdf6af2fb83e0987418ceef81b54
Victor Duarte de Freitas <victorduartedefreitas@gmail.com>
2021-03-28 13:15:58 -0300

    adicionado sql para insercao base de registros

---
+++ sql/base_insert.sql
@@ -0,0 +1,7 @@
+use Toro
+go
+
+insert into Account(Cpf, Name, Bank, Branch, AccountNumber, Balance)
+values ('45358996060', 'Fulano', '352', '0001', '300123', 1030.55),
+('50562883061', 'Ciclano', '352', '0001', '412055', 2355.72),
+('56641234002', 'Beltrano', '352', '0001', '123456', 305.05)
